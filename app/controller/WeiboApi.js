82141087e499be5f132c7f802a9a11502706fe59
cywww <caoyu_92@126.com>
2019-08-23 23:09:28 +0800

    feat：上线提醒

--- app/controller/WeiboApi.js
+++ app/controller/WeiboApi.js
@@ -1,4 +1,4 @@
-import moment from 'moment'
+import moment from 'moment';
 
 class WeiboApi {
     constructor() {
@@ -7,16 +7,26 @@ class WeiboApi {
 
     get531status(ctx) {
         return new Promise(async resolve => {
-
             const card = await ctx.service.weiboApi.get531status();
+            const now = moment().format('YYYY-MM-DD HH:mm:ss');
+
+            if (card === '微博在线了') {
+                ctx.service.mail.send(
+                    ctx.config.gm,
+                    `吴宣仪上线了-${now}`,
+                    `
+                        <p>上线了！${now}</p>
+                    `
+                );
+            }
 
             ctx.response = {
                 errno: 0,
                 msg: 'success',
                 data: {
-                    time: moment().format('YYYY-MM-DD HH:mm:ss'),
-                    desc: card,
-                },
+                    time: now,
+                    desc: card
+                }
             };
             resolve();
         });
